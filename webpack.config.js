277165eda7f77afe028eb3faa586d30236247298
andronSeiko <baklanishe1337@gmail.com>
2024-12-08 02:27:06 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
+        }),
+    ],
+};
